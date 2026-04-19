cf73fa4593d0062bfe4227b188d4d53b8f6c9ded
Alexander <info@kaiser-alexander.de>
2026-04-19 13:10:27 +0200

    feat: move WPB2Elementor to top-level admin menu

--- includes/class-admin-ui.php
+++ includes/class-admin-ui.php
@@ -12,11 +12,11 @@ class WPB2EL_Admin_UI {
     }
 
     public function register_menu(): void {
-        add_management_page( 'WPB2Elementor', 'WPB2Elementor', 'manage_options', 'wpb2elementor', [ $this, 'render_page' ] );
+        add_menu_page( 'WPB2Elementor', 'WPB2Elementor', 'manage_options', 'wpb2elementor', [ $this, 'render_page' ], 'dashicons-migrate', 2 );
     }
 
     public function enqueue_assets( string $hook ): void {
-        if ( $hook !== 'tools_page_wpb2elementor' ) return;
+        if ( $hook !== 'toplevel_page_wpb2elementor' ) return;
         wp_enqueue_style( 'wpb2el-admin', WPB2EL_URL . 'assets/admin.css', [], WPB2EL_VERSION );
     }
 
